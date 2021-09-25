900a3f9f1ce374fe054a4302cb2eec1f0b4e5bdf
tterimaa <tterimaa@gmail.com>
2021-09-25 14:49:12 +0300

    ESLint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import input from "./input.json"
-
-interface Input {
-    prop1: string,
-    prop2: number
-}
+import input from './input.json'
 
 /**
  * Print days of a given month
@@ -11,15 +6,14 @@ interface Input {
  * @param days 1-(28-31)
  */
 const printDaysOfMonth = (month: number) => {
-    const first = new Date(2021, month - 1); // Date API months range 0-11
-    const last = new Date(2021, month, 0); // Day 0 gives last day of previous month
-    
-    let current = first;
-    while (current <= last) {
-        console.log(current.toLocaleDateString('fi-FI'))
-        current.setDate(current.getDate() + 1)
-    }
+  const first = new Date(2021, month - 1) // Date API months range 0-11
+  const last = new Date(2021, month, 0) // Day 0 gives last day of previous month
 
+  const current = first
+  while (current <= last) {
+    console.log(current.toLocaleDateString('fi-FI'))
+    current.setDate(current.getDate() + 1)
+  }
 }
 
 printDaysOfMonth(input.prop2)
