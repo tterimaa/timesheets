46858dcc68e7ba2ee118df10d29201a0421693e7
tterimaa <tterimaa@gmail.com>
2021-09-25 13:04:24 +0300

    Iterate and print days of given month

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,21 @@ interface Input {
     prop2: number
 }
 
-const printTestObject = (testInput: Input) => {
-    console.log(testInput.prop1);
-    console.log(testInput.prop2);
+/**
+ * Print days of a given month
+ * @param month 1-12
+ * @param days 1-(28-31)
+ */
+const printDaysOfMonth = (month: number) => {
+    const first = new Date(2021, month - 1); // Date API months range 0-11
+    const last = new Date(2021, month, 0); // Day 0 gives last day of previous month
+    
+    let current = first;
+    while (current <= last) {
+        console.log(current.toLocaleDateString('fi-FI'))
+        current.setDate(current.getDate() + 1)
+    }
+
 }
 
-printTestObject(input)
+printDaysOfMonth(input.prop2)
