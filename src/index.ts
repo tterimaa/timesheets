0fb532062ad9cbe4c6d02c6b46e4bbf0de9d9cfc
tterimaa <tterimaa@gmail.com>
2021-09-30 10:44:15 +0300

    Evening hours formula

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import input from './input.json'
 import { generateWorkBook } from './workbookGenerator.js'
 
-interface Input {
-  prop1: string,
-  prop2: number
-}
-
 const workbook = generateWorkBook(input.prop2)
 workbook.xlsx.writeFile('./workbooks/workbook.xlsx')
--- src/workbookGenerator.ts
+++ src/workbookGenerator.ts
@@ -23,6 +23,7 @@ const START_ROW = 4
 const GAP = 5
 
 const dayHoursFormula = (start: string, finish: string) => `IF(${start}>18,0,IF(${finish}<=18,${finish}-${start},IF(${finish}>18,18-${start},0)))`
+const eveningHoursFormula = (start: string, finish: string) => `IF(${start}>18,${finish}-${start},IF(${finish}>18,${finish}-18,0))`
 
 const getFirstAndLastDaysOfMonth = (month: number) => {
   const first = new Date(2021, month - 1) // Date API months range 0-11
@@ -76,6 +77,13 @@ const writeDayHoursFunction = (sheet: Worksheet, col: string, row: number) => {
   sheet.mergeCells(col + (row + 3), getNextColumn(col) + (row + 3))
 }
 
+const writeEveningHoursFunction = (sheet: Worksheet, col: string, row: number) => {
+  const cell = sheet.getCell(col + (row + 4))
+  const formula = eveningHoursFormula(col + (row + 2), getNextColumn(col) + (row + 2))
+  cell.value = { formula , date1904: false, result: 0 }
+  sheet.mergeCells(col + (row + 4), getNextColumn(col) + (row + 4))
+}
+
 const writeSheet = (sheet: Worksheet, firstDay: Date, lastDay: Date) => {
   const current = firstDay
   let row = START_ROW
@@ -86,6 +94,7 @@ const writeSheet = (sheet: Worksheet, firstDay: Date, lastDay: Date) => {
     styleDateCell(sheet, cell, col, row)
     writeStartFinish(sheet, col, row)
     writeDayHoursFunction(sheet, col, row)
+    writeEveningHoursFunction(sheet, col, row)
     if (col === DAYS_TO_COL.SUNDAY) {
       row = row + GAP
     }
