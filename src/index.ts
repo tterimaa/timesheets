976fa02944631086dc75a55c56aa4a99e73df1ac
tterimaa <tterimaa@gmail.com>
2021-12-08 16:31:30 +0200

    Remove json input file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import input from './input.json'
 import { generateWorkBook } from './workbookGenerator.js'
 
-const workbook = generateWorkBook(input.prop2, ['John', 'Jane'])
+const workbook = generateWorkBook(10, ['John', 'Jane'])
 workbook.xlsx.writeFile('./workbooks/workbook.xlsx')
